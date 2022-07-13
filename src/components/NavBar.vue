0bb54d60e9b11688d895aa7b9bfbaf32e2486eb4
STLnick <nickrray@gmail.com>
2022-07-12 23:25:30 -0500

    Add very basic NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps(["name"]);
+</script>
+
+<template>
+  <div class="w-screen bg-emerald-500 py-3 px-6 text-lg tracking-wide">
+    <span class="">{{ name }}</span>
+  </div>
+</template>
