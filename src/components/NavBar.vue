84cb2ee3c8fdee893d6f727fabce59e71cf2756e
Nick <nickrray@gmail.com>
2022-07-13 22:36:38 -0500

    Change layout to have tabs and dark mode toggle

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,36 @@
 <script setup>
-const props = defineProps(["name"]);
+import { ref } from 'vue';
+import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue';
+import MoonSVG from '@/icons/MoonSVG.vue';
+
+const darkMode = ref(false);
 </script>
 
 <template>
-  <div class="w-screen bg-emerald-500 py-3 px-6 text-lg tracking-wide">
-    <span class="">{{ name }}</span>
+  <div class="w-screen bg-emerald-500 py-3 px-6 text-lg tracking-wide flex justify-between">
+    <div>
+      <ul class="flex text-white">
+        <li class="mr-6">Work</li>
+        <li class="mr-6">Me</li>
+        <li>Fun</li>
+      </ul>
+    </div>
+    <SwitchGroup>
+      <div class="flex items-center">
+        <SwitchLabel class="text-sm italic pr-2">
+          <MoonSVG class="text-white" :class="{ filled: darkMode }" />
+        </SwitchLabel>
+        <Switch
+          v-model="darkMode"
+          :class="darkMode ? 'bg-teal-900' : 'bg-teal-700'"
+          class="relative inline-flex h-6 w-11 items-center rounded-full"
+        >
+          <span
+            :class='darkMode ? "translate-x-6" : "translate-x-1"'
+            class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform"
+          />
+        </Switch>
+      </div>
+    </SwitchGroup>
   </div>
 </template>
