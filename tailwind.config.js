3f490848eb489ccedeef3a24d4c85777c4060665
Nick <nickrray@gmail.com>
2022-07-13 23:38:49 -0500

    Add Vue green to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'vuegreen': {
+          light: '#a0e9c8',
+          DEFAULT: '#42d392',
+          dark: '#1a7049',
+        }
+      },
       fontFamily: {
         sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
       },
