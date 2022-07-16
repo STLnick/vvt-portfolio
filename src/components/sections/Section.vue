51f46dc396f7a9642e735695bfff344051e099ad
Nick <nickrray@gmail.com>
2022-07-16 13:31:14 -0500

    Darken even section backgrounds

--- src/components/sections/Section.vue
+++ src/components/sections/Section.vue
@@ -3,7 +3,7 @@ const props = defineProps([ 'caption', 'heading' ]);
 </script>
 
 <template>
-  <div :class="`max-w-full flex justify-center bg-gray-50 odd:bg-gray-300 dark:bg-secondary-dark dark:odd:bg-secondary py-16 px-8`">
+  <div :class="`max-w-full flex justify-center bg-gray-100 odd:bg-gray-300 dark:bg-secondary-dark dark:odd:bg-secondary py-16 px-8`">
     <div class="w-full max-w-4xl">
       <h2 class="text-4xl text-primary-dark dark:text-primary-light underline underline-offset-4 tracking-widest">
         {{ heading }}
