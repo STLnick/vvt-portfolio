8dd6dca39d0ee24c58c202f3e2a8c2357b8dd3f1
Nick <nickrray@gmail.com>
2022-07-16 10:25:21 -0500

    Toggle dark mode based on cookie on mount

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { nextTick, onMounted } from "vue";
 import NavBar from "@/components/NavBar.vue";
+import { DARK_MODE_COOKIE, getCookie, toggleDarkClass } from "@/utils";
+
+onMounted(() => {
+  if (
+    !document.getElementById('app').classList.contains('dark')
+    && getCookie(DARK_MODE_COOKIE) === 'true'
+  ) {
+    toggleDarkClass(true);
+  }
+});
 </script>
 
 <template>
