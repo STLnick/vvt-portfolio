a0eb8aa317505d16c9f51a1d3c4ba1d7e77211d4
STLnick <nickrray@gmail.com>
2022-10-02 19:21:56 -0500

    Upgrade deps and fix icon imports

--- src/components/TabPanelItem.vue
+++ src/components/TabPanelItem.vue
@@ -1,13 +1,12 @@
 <script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from '@headlessui/vue';
-import { ChevronUpIcon, ExternalLinkIcon } from '@heroicons/vue/solid';
-import { TransitionRoot } from '@headlessui/vue'
+  ChevronUpIcon,
+  ArrowTopRightOnSquareIcon,
+} from "@heroicons/vue/20/solid";
+import { TransitionRoot } from "@headlessui/vue";
 
-const props = defineProps([ 'item' ]);
+const props = defineProps(["item"]);
 const item = props.item;
 </script>
 
@@ -15,13 +14,15 @@ const item = props.item;
   <Disclosure v-slot="{ open }">
     <div class="p-4">
       <!-- Panel activator -->
-      <DisclosureButton class="w-full py-2 flex items-center dark:text-accent-light">
+      <DisclosureButton
+        class="flex w-full items-center py-2 dark:text-accent-light"
+      >
         <span class="basis-auto">{{ item.title }}</span>
         <ChevronUpIcon
           :class="open ? 'rotate-180 transform' : ''"
-          class="h-9 w-9 mx-2 fill-secondary-dark dark:fill-accent-light transition-all"
+          class="mx-2 h-9 w-9 fill-secondary-dark transition-all dark:fill-accent-light"
         />
-        <div class="w-2/3 p-px grow bg-secondary-lightest"></div>
+        <div class="w-2/3 grow bg-secondary-lightest p-px"></div>
       </DisclosureButton>
       <!-- Expanded content -->
       <TransitionRoot
@@ -33,18 +34,22 @@ const item = props.item;
         leave-from="opacity-100"
         leave-to="opacity-0"
       >
-        <DisclosurePanel class="text-gray-500 border-l-2 border-l-secondary-light dark:text-secondary-lightest transition-all ml-4 pl-2 pr-8">
+        <DisclosurePanel
+          class="ml-4 border-l-2 border-l-secondary-light pl-2 pr-8 text-gray-500 transition-all dark:text-secondary-lightest"
+        >
           <a
             v-if="item.link"
             :class="[
-              'flex align-center mt-1 mb-2',
+              'align-center mt-1 mb-2 flex',
               'text-primary underline decoration-primary hover:text-primary-dark hover:decoration-primary-dark',
-              'dark:text-accent dark:decoration-accent dark:hover:text-accent-dark hover:dark:decoration-accent-dark'
+              'dark:text-accent dark:decoration-accent dark:hover:text-accent-dark hover:dark:decoration-accent-dark',
             ]"
             :href="item.link.href"
           >
-            {{item.link.text}}
-            <ExternalLinkIcon class="h-5 w-5 my-auto fill-primary-dark dark:fill-accent-dark" />
+            {{ item.link.text }}
+            <ArrowTopRightOnSquareIcon
+              class="my-auto h-5 w-5 fill-primary-dark dark:fill-accent-dark"
+            />
           </a>
           <p v-html="item.content.text"></p>
         </DisclosurePanel>
@@ -53,6 +58,4 @@ const item = props.item;
   </Disclosure>
 </template>
 
-<style>
-
-</style>
+<style></style>
